aeeb122a55c9e731d650e14b1d448d5255c1f3d6
gnikoloudakis <hose.cuervo@gmail.com>
2016-12-13 14:20:23 +0200

    updated python script

--- sip_proxy.py
+++ sip_proxy.py
@@ -11,19 +11,17 @@ command_1 = """sip-settings -a add %s %s""" % (userAccount, userPassword)
 os.system(command_1)
 
 
-@app.route('/', methods=['POST'])
-def hello_world():
+@app.route('/', methods=['GET', 'POST'])
+def index():
     global userName
     data = json.loads(request.data)
     # print (data['sender'])
     recipient = data['recipient']
     message = data['message']
-
     command_2 = """sip-message -a %s %s -m "%s" """ % (userName, recipient, message)
-
     os.system(command_2)
-    return 'Hello World!'
+    return 'Message Sent!'
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=9090)
